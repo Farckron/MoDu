150614800e517015f6bab0c116496c923def37b3
Serhii Voitov <144520827+Farckron@users.noreply.github.com>
2025-08-09 16:32:41 -0700

    initial commit

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "habit-tracker-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const { request } = e;
+  e.respondWith(
+    caches.match(request).then(cached => cached || fetch(request))
+  );
+});
